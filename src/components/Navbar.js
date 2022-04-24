c2d4ee71e6838564aee7f047419d10251f19c855
AlexandruNeculaes <alexandru.neculaes@gmail.com>
2022-04-24 23:27:05 +0300

    bootstrap home page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,54 +14,70 @@ import logo3 from "../Images/small_logo.jpg";
 
 function Navbar() {
   return (
-    <div className="topnav" id="myTopnav">
-      {/* using fontawsome added the website smaller logo, the star */}
-
-      <div>
-        <FontAwesomeIcon className="starIcon" icon={faStar} />
-      </div>
-
-      {/* added website main logo*/}
-
-      <div>
-        <img src={logo3} />
-      </div>
-
-      {/* done pathing for the links */}
-
-      <div className="navItems">
-        <Link className="navItem" to="/">
-          Home
-        </Link>
-        <Link className="navItem" to="/menu">
-          Menu
-        </Link>
-        <Link className="navItem" to="/about">
-          About
-        </Link>
-        <Link className="navItem" to="/contact">
-          Contact
-        </Link>
-      </div>
-
-      {/* using fontawsome added the social media logos and set correct path to websites */}
-      {/*once clicked a new page will open */}
-
-      <div className="socialmediaIcons">
-        <a href="https://google.com" target="_blank">
-          <FontAwesomeIcon className="socialIcon" icon={faFacebook} />
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark navBarBlack">
+      <div className="container-fluid">
+        {/* using fontawsome added the website smaller logo, the star */}
+        <a className="navbar-brand" href="#">
+          <FontAwesomeIcon className="starIcon" icon={faStar} />
         </a>
-        <a href="https://google.com" target="_blank">
-          <FontAwesomeIcon className="socialIcon" icon={faLinkedin} />
-        </a>
-        <a href="https://google.com" target="_blank">
-          <FontAwesomeIcon className="socialIcon" icon={faYelp} />
-        </a>
-        <a href="https://google.com" target="_blank">
-          <FontAwesomeIcon className="socialIcon" icon={faGoogle} />
-        </a>
+        {/* added website main logo*/}
+        <div className="navbar-brand">
+          <img src={logo3} />
+        </div>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            {/* done pathing for the links */}
+            <li className="nav-item">
+              <Link className="nav-link active navItem" to="/">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link navItem" to="/menu">
+                Menu
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link navItem" to="/about">
+                About
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link navItem" to="/contact">
+                Contact
+              </Link>
+            </li>
+          </ul>
+          {/* using fontawsome added the social media logos and set correct path to websites */}
+          {/*once clicked a new page will open */}
+          <div className="d-flex">
+            <a href="https://www.facebook.com/" target="_blank">
+              <FontAwesomeIcon className="socialIcon" icon={faFacebook} />
+            </a>
+            <a href="https://www.linkedin.com/" target="_blank">
+              <FontAwesomeIcon className="socialIcon" icon={faLinkedin} />
+            </a>
+            <a href="https://www.yelp.ie/" target="_blank">
+              <FontAwesomeIcon className="socialIcon" icon={faYelp} />
+            </a>
+            <a href="https://google.com" target="_blank">
+              <FontAwesomeIcon className="socialIcon" icon={faGoogle} />
+            </a>
+          </div>
+        </div>
       </div>
-    </div>
+    </nav>
   );
 }
 
